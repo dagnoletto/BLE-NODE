4168ebbc65095fa5c9c935f4cb465e28f6c8d7d0
dagnoletto <daagno@hotmail.com>
2021-01-31 00:03:34 -0300

    Processor runing speed changed to 32MHz to achieve highest SPI speed for the BLE module (8MHz)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -99,11 +99,12 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  if( TimeBase_DelayMs( &Timer, 1000UL, TRUE) )
+
+    /* USER CODE BEGIN 3 */
+	  if( TimeBase_DelayMs( &Timer, 1000UL, TRUE ) )
 	  {
 		  HAL_GPIO_TogglePin( HEART_BEAT_GPIO_Port, HEART_BEAT_Pin );
 	  }
-    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
@@ -124,7 +125,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL4;
   RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
