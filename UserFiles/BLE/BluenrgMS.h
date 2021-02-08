2f3c22a6c686788bd32eaff89d25de85f3f63e81
dagnoletto <daagno@hotmail.com>
2021-02-07 23:03:08 -0300

    HCI packet types.

--- UserFiles/BLE/BluenrgMS.h
+++ UserFiles/BLE/BluenrgMS.h
@@ -15,6 +15,23 @@
 /****************************************************************/
 /* Type Defines                                                 */
 /****************************************************************/
+/* Those are the packet types defined in the Bluetooth Core Specification */
+/* The standard defines the UART Transport Layer in v5.2 Volume 4: Part A */
+typedef enum
+{
+	HCI_COMMAND_PACKET 			= 0x01,
+	HCI_ACL_DATA_PACKET 		= 0x02,
+	HCI_SYNCHRONOUS_DATA_PACKET = 0x03,
+	HCI_EVENT_PACKET			= 0x04,
+	HCI_ISO_DATA_PACKET 		= 0x05
+}HCI_PACKET_TYPE;
+
+
+typedef enum
+{
+	SPI_WRITE,
+	SPI_READ,
+}BLUENRG_COMMAND;
 
 
 /****************************************************************/
