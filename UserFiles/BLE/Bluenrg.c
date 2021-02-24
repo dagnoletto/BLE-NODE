ca9d99ad1718e69163b8f385183f4b916251f6f0
dagnoletto <daagno@hotmail.com>
2021-02-23 23:01:20 -0300

    The Bluenrg header response was improved. Counters were added to put the driver in hold mode to avoid continuous header requests that would stall the host.

--- UserFiles/BLE/Bluenrg.c
+++ UserFiles/BLE/Bluenrg.c
@@ -13,6 +13,11 @@
 #define CTRL_READ  	  0x0B
 #define DEVICE_READY  0x02
 #define SIZE_OF_LOCAL_MEMORY_BUFFER 255
+#define DEVICE_NOT_READY_THRESHOLD	  3 /* Number of not ready responses in sequence to put device in hold mode for DEVICE_HOLD_TIME */
+#define NO_ALLOWED_WRITE_THRESHOLD	  3 /* Number of not allowed write responses (in sequence) to put device in hold mode for DEVICE_HOLD_TIME */
+#define ERRONEOUS_RESPONSE_THRESHOLD  3 /* Number of erroneous responses (in sequence) to put device in hold mode for DEVICE_HOLD_TIME */
+#define DEVICE_HOLD_TIME			  20UL /* Time in ms to wait until frames are sent to the device again after
+											device not ready for DEVICE_NOT_READY_THRESHOLD times */
 #define SIZE_OF_FRAME_BUFFER 		  8
 #define SIZE_OF_CALLBACK_BUFFER 	  4
 
@@ -64,6 +69,8 @@ typedef struct
 {
 	uint16_t AllowedWriteSize;
 	uint16_t SizeToRead;
+	uint32_t HoldTime;
+	uint32_t HoldCounter;
 	int8_t NumberOfFilledBuffers;
 	BUFFER_DESC* BufferHead; /* The head always points to the buffer to be transmitted first */
 	BUFFER_DESC* BufferTail; /* The tail always points to a free buffer */
@@ -190,6 +197,16 @@ void Run_Bluenrg(void)
 
 			Release_CallBack();
 		}
+
+		/* If Hold time is active, no more messages can be sent for the duration of hold */
+		if( BufferManager.HoldTime )
+		{
+			if( TimeBase_DelayMs( &BufferManager.HoldCounter, BufferManager.HoldTime, TRUE) )
+			{
+				BufferManager.HoldTime = 0;
+				Request_Frame();
+			}
+		}
 	}
 }
 
@@ -228,6 +245,8 @@ static void Init_Buffer_Manager(void)
 		BufferManager.Buffer[i].Status = BUFFER_FREE; /* Buffer is free */
 	}
 
+	BufferManager.HoldTime = 0;
+	BufferManager.HoldCounter = 0;
 	BufferManager.BufferHead = &BufferManager.Buffer[0]; /* The head always points to the buffer to be transmitted first */
 	BufferManager.BufferHead->Next = &BufferManager.Buffer[0]; /* Points to the next buffer for transmission */
 
@@ -588,85 +607,88 @@ static void Request_Frame(void)
 
 	CheckBufferHead:
 
-	if( ( BufferManager.BufferHead->Status != BUFFER_FREE ) && ( BufferManager.BufferHead->Status != BUFFER_TRANSMITTING ) )
+	if( !BufferManager.HoldTime ) /* We are not holding */
 	{
-		switch( BufferManager.BufferHead->TransferMode )
+		if( ( BufferManager.BufferHead->Status != BUFFER_FREE ) && ( BufferManager.BufferHead->Status != BUFFER_TRANSMITTING ) )
 		{
+			switch( BufferManager.BufferHead->TransferMode )
+			{
 
-		case SPI_WRITE:
-			/* When the write is called for the first time, the slave header read is requested to update AllowedWriteSize */
-			if( ( BufferManager.AllowedWriteSize == 0 ) || ( BufferManager.BufferHead->Status == BUFFER_FULL ) )
-			{
-				BufferManager.BufferHead->Status = BUFFER_PAUSED;
-				Request_Slave_Header( SPI_HEADER_WRITE );
-				goto CheckBufferHead; /* I know, I know, ugly enough. But think of code savings and performance, OK? */
-			}else
-			{
-				/* We can write the data, but check if all bytes can be sent at once or not */
-				if( BufferManager.BufferHead->RemainingBytes > BufferManager.AllowedWriteSize )
+			case SPI_WRITE:
+				/* When the write is called for the first time, the slave header read is requested to update AllowedWriteSize */
+				if( ( BufferManager.AllowedWriteSize == 0 ) || ( BufferManager.BufferHead->Status == BUFFER_FULL ) )
 				{
-					DataSize = BufferManager.AllowedWriteSize;
+					BufferManager.BufferHead->Status = BUFFER_PAUSED;
+					Request_Slave_Header( SPI_HEADER_WRITE );
+					goto CheckBufferHead; /* I know, I know, ugly enough. But think of code savings and performance, OK? */
 				}else
 				{
-					DataSize = BufferManager.BufferHead->RemainingBytes;
-				}
+					/* We can write the data, but check if all bytes can be sent at once or not */
+					if( BufferManager.BufferHead->RemainingBytes > BufferManager.AllowedWriteSize )
+					{
+						DataSize = BufferManager.AllowedWriteSize;
+					}else
+					{
+						DataSize = BufferManager.BufferHead->RemainingBytes;
+					}
 
-				BufferManager.BufferHead->TxPtr += BufferManager.BufferHead->Counter;
+					BufferManager.BufferHead->TxPtr += BufferManager.BufferHead->Counter;
+					TXDataPtr = BufferManager.BufferHead->TxPtr;
+
+					BufferManager.BufferHead->RemainingBytes -= DataSize;
+					BufferManager.BufferHead->Counter += DataSize;
+
+					/* Force the reading of a new header before every new transfer just to make sure the module is ready
+					 * and has valid buffer space to accept data */
+					BufferManager.AllowedWriteSize = 0;
+				}
+				break;
+
+			case SPI_READ:
+				DataSize = BufferManager.BufferHead->RemainingBytes;
 				TXDataPtr = BufferManager.BufferHead->TxPtr;
 
-				BufferManager.BufferHead->RemainingBytes -= DataSize;
-				BufferManager.BufferHead->Counter += DataSize;
-
-				/* Force the reading of a new header before every new transfer just to make sure the module is ready
-				 * and has valid buffer space to accept data */
-				BufferManager.AllowedWriteSize = 0;
-			}
-			break;
-
-		case SPI_READ:
-			DataSize = BufferManager.BufferHead->RemainingBytes;
-			TXDataPtr = BufferManager.BufferHead->TxPtr;
-
-			if( ( BufferManager.SizeToRead == 0 ) || ( DataSize < BufferManager.SizeToRead ) )
-			{
-				/* If Size to read is zero or available buffer size is lower than the necessary, trigger an Error because we do not consider
-				 * partial transfer at transport layer. Also, the read is always triggered by Bluenrg IRQ pin. So, it is expected SizeToRead to be non zero
-				 * whenever this code is reached. */
-				/* Here we consider that all read bytes belongs to the same information being reported. That is, when a read is available, it is assumed
+				if( ( BufferManager.SizeToRead == 0 ) || ( DataSize < BufferManager.SizeToRead ) )
+				{
+					/* If Size to read is zero or available buffer size is lower than the necessary, trigger an Error because we do not consider
+					 * partial transfer at transport layer. Also, the read is always triggered by Bluenrg IRQ pin. So, it is expected SizeToRead to be non zero
+					 * whenever this code is reached. */
+					/* Here we consider that all read bytes belongs to the same information being reported. That is, when a read is available, it is assumed
 				that all bytes regarding to a packet type (HCI event packet, for example) will be transferred in a single read transfer. The Bluenrg does
 				specify a read transfer protocol(? TODO) to deal with partial transfer at transport layer. The maximum parameter size according to BLE is
 				one byte long. So, the maximum message size will be 255 bytes excluding the header size. However, the ACL_Data_Packet has a data length
 				parameter of 16 bits long. Therefore, for ACL_Data_Packets in the future, a transport protocol might be needed. */
 
-				Bluenrg_Error( READ_ARGS_ERROR );
+					Bluenrg_Error( READ_ARGS_ERROR );
+
+					return;
+				}
+				break;
+
+			case SPI_HEADER_READ:
+			case SPI_HEADER_WRITE:
+				/* No restrictions about SPI header read since it is used to get slave status */
+				DataSize = BufferManager.BufferHead->RemainingBytes;
+				TXDataPtr = BufferManager.BufferHead->TxPtr;
+
+				break;
+
+			default:
+
+				Bluenrg_Error( UNKNOWN_ERROR );
 
 				return;
+
+				break;
 			}
-			break;
 
-		case SPI_HEADER_READ:
-		case SPI_HEADER_WRITE:
-			/* No restrictions about SPI header read since it is used to get slave status */
-			DataSize = BufferManager.BufferHead->RemainingBytes;
-			TXDataPtr = BufferManager.BufferHead->TxPtr;
+			BufferManager.BufferHead->Status = BUFFER_TRANSMITTING;
 
-			break;
-
-		default:
-
-			Bluenrg_Error( UNKNOWN_ERROR );
-
-			return;
-
-			break;
-		}
-
-		BufferManager.BufferHead->Status = BUFFER_TRANSMITTING;
-
-		if( Bluenrg_Send_Frame( BufferManager.BufferHead->TransferMode, TXDataPtr,
-				BufferManager.BufferHead->RxPtr, DataSize ) == FALSE )
-		{
-			Release_Bluenrg();
+			if( Bluenrg_Send_Frame( BufferManager.BufferHead->TransferMode, TXDataPtr,
+					BufferManager.BufferHead->RxPtr, DataSize ) == FALSE )
+			{
+				Release_Bluenrg();
+			}
 		}
 	}
 }
@@ -709,7 +731,7 @@ void Bluenrg_Frame_Status(TRANSFER_STATUS status)
 
 				if( TransferDescPtr->CallBackMode == CALL_BACK_AFTER_TRANSFER )
 				{
-					if( BufferManager.BufferHead->TransferMode == SPI_HEADER_READ || BufferManager.BufferHead->TransferMode == SPI_HEADER_WRITE )
+					if( ( BufferManager.BufferHead->TransferMode == SPI_HEADER_READ ) || ( BufferManager.BufferHead->TransferMode == SPI_HEADER_WRITE ) )
 					{
 
 						ReleaseSPI = TransferDescPtr->CallBack( TransferDescPtr, BufferManager.BufferHead->TransferMode, BufferManager.BufferHead->TransferStatus );
@@ -824,10 +846,16 @@ static BUFFER_DESC* Search_For_Free_Frame(void)
 /****************************************************************/
 static uint8_t Slave_Header_CallBack(TRANSFER_DESCRIPTOR* Transfer, SPI_TRANSFER_MODE HeaderMode, TRANSFER_STATUS Status)
 {
+	uint8_t static DeviceNotReadyCounter = 0;
+	uint8_t static NoAllowedWriteCounter = 0;
+	uint8_t static ErroneousResponseCounter = 0;
+
 	SPI_SLAVE_HEADER* Header = (SPI_SLAVE_HEADER*)( Transfer->DataPtr );
 
 	if( Header->READY == DEVICE_READY )
 	{
+		DeviceNotReadyCounter = 0;
+
 		BufferManager.AllowedWriteSize = Header->WBUF;
 		BufferManager.SizeToRead = Header->RBUF;
 
@@ -836,6 +864,8 @@ static uint8_t Slave_Header_CallBack(TRANSFER_DESCRIPTOR* Transfer, SPI_TRANSFER
 		   Request_Frame function we should make sure to enqueue a header read request before the reading. */
 		if( BufferManager.SizeToRead != 0 )
 		{
+			ErroneousResponseCounter = 0;
+			NoAllowedWriteCounter = 0;
 			BufferManager.AllowedWriteSize = 0; /* Just to make sure that an ongoing write transaction will request the header before restart */
 
 			/* Enqueue a read command at the second buffer position (index == 1) and do not release the SPI */
@@ -843,29 +873,50 @@ static uint8_t Slave_Header_CallBack(TRANSFER_DESCRIPTOR* Transfer, SPI_TRANSFER
 
 			return (DO_NOT_RELEASE_SPI); /* For the read operation, keeps device asserted and send dummy bytes MOSI */
 
-		}else if( ( HeaderMode == SPI_HEADER_WRITE ) && ( BufferManager.AllowedWriteSize != 0 ) )
+		}else if( HeaderMode == SPI_HEADER_WRITE )
 		{
+			ErroneousResponseCounter = 0;
 
-			return (DO_NOT_RELEASE_SPI); /* For the write operation, keeps device asserted to send payload bytes */
-
+			if( BufferManager.AllowedWriteSize != 0 )
+			{
+				NoAllowedWriteCounter = 0;
+				return (DO_NOT_RELEASE_SPI); /* For the write operation, keeps device asserted to send payload bytes */
+			}else
+			{
+				NoAllowedWriteCounter++;
+				if( NoAllowedWriteCounter >= NO_ALLOWED_WRITE_THRESHOLD )
+				{
+					BufferManager.HoldTime = DEVICE_HOLD_TIME; /* hold time */
+					BufferManager.HoldCounter = 0;	/* From now */
+				}
+			}
 		}else
 		{
-			/* TODO: Talvez colocar um proteção ou um tempo aleatório para uma nova tentativa ao invés de imediatamente
-			 * requisitar uma outra leitura, pois pode ficar indo e voltado freneticamente da interrupção caso o dispositivo
-			 * não tenha SizeToRead tão logo (ou nunca). Fazer isto no loop principal e não aqui nesta interrupção. Fazer isso
-			 * em função da quantidade de buffers agendados para transmissão: se só 1 (este) estiver agendado, poderá ser requisitado
-			 * um tempo mais tarde. Caso o buffer esteja com mais de um, pode ser requisitado aqui */
+			NoAllowedWriteCounter = 0;
+
 			if( Get_Bluenrg_IRQ_Pin() ) /* Check if the IRQ pin is set */
 			{
 				/* Enqueue a new slave header read to check if device has bytes to read */
 				Request_Slave_Header( SPI_HEADER_READ );
 			}
+
+			ErroneousResponseCounter++;
+			if( ErroneousResponseCounter >= ERRONEOUS_RESPONSE_THRESHOLD )
+			{
+				BufferManager.HoldTime = DEVICE_HOLD_TIME; /* hold time */
+				BufferManager.HoldCounter = 0;	/* From now */
+			}
 		}
 	}else
 	{
-		/* TODO: Talvez colocar um proteção ou um tempo aleatório para uma nova tentativa ao invés de imediatamente
-		 * requisitar uma outra leitura, pois pode ficar indo e voltado freneticamente da interrupção caso o dispositivo
-		 * não fique DEVICE_READY tão logo (ou nunca). Fazer isto no loop principal e não aqui nesta interrupção. */
+		/* If the SPI clock frequency to access the device is too high, it is necessary to read the header
+		 * more times to get the ready status. Experiences with the BlueNRG-MS showed that above 500kbps,
+		 * the DeviceNotReadyCounter increases up to two until DEVICE_READY is achieved. However, if very low
+		 * frequency is used, you cannot achieve the best BLE throughput of 1 Mbps */
+
+		NoAllowedWriteCounter = 0;
+		ErroneousResponseCounter = 0;
+
 		BufferManager.AllowedWriteSize = 0;
 		BufferManager.SizeToRead = 0;
 
@@ -873,6 +924,13 @@ static uint8_t Slave_Header_CallBack(TRANSFER_DESCRIPTOR* Transfer, SPI_TRANSFER
 		{
 			/* Enqueue a new slave header read to check if device is ready */
 			Request_Slave_Header( SPI_HEADER_READ );
+		}
+
+		DeviceNotReadyCounter++;
+		if( DeviceNotReadyCounter >= DEVICE_NOT_READY_THRESHOLD )
+		{
+			BufferManager.HoldTime = DEVICE_HOLD_TIME; /* hold time */
+			BufferManager.HoldCounter = 0;	/* From now */
 		}
 	}
 
